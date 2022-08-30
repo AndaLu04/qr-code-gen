72c2ac8ea253883de9819bb4e490eefc8fc87208
luise <lerbert@lotus-services.de>
2022-08-30 10:37:52 +0200

    improvements

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import os.path
-import pathlib
-
-from flask import Flask, render_template, request
+import io
+import base64
 import qrcode
+
+from flask import Flask, render_template, request,make_response, send_file
+from PIL import Image
+from io import BytesIO
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import SquareModuleDrawer
 from qrcode.image.styles.moduledrawers import GappedSquareModuleDrawer
@@ -10,17 +12,16 @@ from qrcode.image.styles.moduledrawers import CircleModuleDrawer
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from qrcode.image.styles.moduledrawers import VerticalBarsDrawer
 from qrcode.image.styles.moduledrawers import HorizontalBarsDrawer
-from pathlib import Path
 
 
-def render_code(value, module_drawer='default', resolution='default', size=10, fill_color='black', back_color='white'):
+def render_code(value, module_drawer='default', resolution='default', size=5, fill_color='black', back_color='white'):
     drawerlist = {
         'default': SquareModuleDrawer(),
-        'gapped': GappedSquareModuleDrawer,
-        'circle': CircleModuleDrawer,
-        'rounded': RoundedModuleDrawer,
-        'vertical': VerticalBarsDrawer,
-        'horizontal': HorizontalBarsDrawer
+        'gapped': GappedSquareModuleDrawer(),
+        'circle': CircleModuleDrawer(),
+        'rounded': RoundedModuleDrawer(),
+        'vertical': VerticalBarsDrawer(),
+        'horizontal': HorizontalBarsDrawer()
     }
 
     resolutionlist = {
@@ -42,14 +43,16 @@ def render_code(value, module_drawer='default', resolution='default', size=10, f
     qr.add_data(value)
     img = qr.make_image(
         image_factory=StyledPilImage,
-        moduledrawer=drawer,
+        module_drawer=drawer,
         embeded_image_path=emb_img,
         back_color=back_color,
         fill_color=fill_color
     )
-    type(img)
-    img.save(f'{pathlib.Path().absolute()}/static/img/code.png')
-    return True
+    img_mem = io.BytesIO()
+    img.save(img_mem, "PNG")
+    encoded_img_mem = base64.b64encode(img_mem.getvalue())
+
+    return encoded_img_mem.decode("utf-8")
 
 
 app = Flask(__name__)
@@ -60,10 +63,43 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/code', methods=['GET', 'POST'])
+@app.route('/preview', methods=['GET', 'POST'])
 def code():
-    if render_code(request.form['value_input']) is True:
-        return render_template('index.html', user_image='/static/img/code.png')
+    value = request.form['value_input']
+    design = request.form['design_input']
+    resolution = request.form['resolution_input']
+    size = request.form['size_input']
+    try:
+        size = int(size)
+        if not 1 <= size <= 40:
+            size = 5
+    except ValueError:
+        size = 5
+    img_data = render_code(value, design, resolution, size)
+    return render_template('index.html',
+                           preview_value=value,
+                           preview_design=design,
+                           preview_resolution=resolution,
+                           preview_size=size,
+                           user_image=img_data
+                           )
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    value = request.form['value_input']
+    design = request.form['design_input']
+    resolution = request.form['resolution_input']
+    size = request.form['size_input']
+    try:
+        size = int(size)
+        if not 1 <= size <= 40:
+            size = 5
+    except ValueError:
+        size = 5
+    img_data = render_code(value, design, resolution, size)
+    img = Image.open(BytesIO(base64.b64decode(img_data)))
+    return
 
 
 if __name__ == '__main__':
